ccac268b6d3c2cd860b08f32f298a86c93df9404
chenxianyu52 <380286681@qq.com>
2021-12-17 14:15:51 +0800

    password 读取环境变量

--- config/config.default.js
+++ config/config.default.js
@@ -16,7 +16,7 @@ module.exports = appInfo => {
         host:'127.0.0.1',
         port:'3306',
         user:'root',
-        password:'Loveaini0714',
+        password:process.env.PASSWORD,
 	database:'test'
       },
       app:true,
